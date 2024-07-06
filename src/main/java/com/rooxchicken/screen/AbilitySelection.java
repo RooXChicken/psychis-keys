b67f077fec119168b2e21df8be4a5909506d8428
RooXChicken <rooxchicken@gmail.com>
2024-07-05 20:42:44 -0400

    Model fixes

--- src/main/java/com/rooxchicken/screen/AbilitySelection.java
+++ src/main/java/com/rooxchicken/screen/AbilitySelection.java
@@ -108,7 +108,7 @@ public class AbilitySelection extends Screen
     public void render(DrawContext context, int mouseX, int mouseY, float delta)
     {
         MinecraftClient client = MinecraftClient.getInstance();
-		this.renderBackground(context, mouseX, mouseY, delta);
+		this.renderBackground(context);
 		
         RenderSystem.enableBlend();
         context.drawTexture(bgTexture, width/2 - 128, height/2 - 128, 0, 0, 256, 256);
